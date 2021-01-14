5664fad3dbb6b1c0586e15bce466d6932486bab6
Oren Panitch <oreokid200@gmail.com>
2021-01-14 16:25:20 -0500

    final changes for MVP, site v1

--- source/components/SiteFooter/SiteFooter.tsx
+++ source/components/SiteFooter/SiteFooter.tsx
@@ -2,9 +2,11 @@ import React, { FunctionComponent } from 'react';
 import { NavLink } from 'react-router-dom';
 import classnames from 'classnames';
 
+import GitHubIcon from 'ASSETS/icons/github.svg';
+
 import mainNavItems from 'CONFIG/main-nav';
 
-import { Header, List, ListItem } from 'Atoms';
+import { Header, Link, List, ListItem } from 'Atoms';
 
 import { FullWidthContainer } from 'Components';
 
@@ -17,7 +19,7 @@ const Footer: FunctionComponent = () => {
             <div className="flex items-start justify-between w-full">
               <nav className="pr-50 md:pr-100 border-t-1">
                 <List
-                  className="flex leading-none font-header text-scale-4"
+                  className="flex pt-1 leading-none font-header text-scale-4"
                   isOrdered={false}
                 >
                   {mainNavItems.map((navItem, index) => {
@@ -39,9 +41,19 @@ const Footer: FunctionComponent = () => {
                 title="O.P"
               />
             </div>
-            <p className="py-2 mt-3 text-right text-scale-n1">
-              &copy; 2021 Oren Panitch
-            </p>
+            <div className="flex items-start justify-between w-full">
+              <Link
+                className="p-2 mt-1 rounded-md text-scale-n1 border-1"
+                href="https://github.com/opanitch/portfolio"
+                target="_blank"
+              >
+                See on <GitHubIcon className="inline-block text-scale-3" />
+              </Link>
+
+              <p className="py-2 mt-3 text-scale-n1">
+                &copy; 2021 Oren Panitch
+              </p>
+            </div>
           </footer>
         </ChildContainer>
       )}
