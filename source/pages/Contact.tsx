050d4893e7a42c234d968dced896de3052c9b4c6
Oren Panitch <oreokid200@gmail.com>
2021-02-24 14:58:59 -0500

    made some changes to Contact page layout with new contact form

--- source/pages/Contact.tsx
+++ source/pages/Contact.tsx
@@ -21,36 +21,39 @@ const ContactPage: FunctionComponent<Page> = ({
   >
     {({ ChildContainer }) => (
       <ChildContainer className="flex items-center justify-center h-full">
-        <Card className="w-5/6 md:w-3/5 lg:w-1/2">
-          <Header
-            className="mb-2"
-            headerLevel={2}
-            title={`{{@ cms.contact.title @}}`}
-          />
-          <p className="font-nugo">{`{{@ cms.contact.description @}}`}</p>
-          <List className="flex justify-center mt-3 leading-none text-scale-6">
-            <ListItem>
-              <Link
-                href="https://www.linkedin.com/in/opanitch/"
-                target="_blank"
-              >
-                <LinkedinIcon />
-              </Link>
-            </ListItem>
-            <ListItem className="ml-4">
-              <Link href="https://github.com/opanitch" target="_blank">
-                <GithubIcon />
-              </Link>
-            </ListItem>
-            <ListItem className="ml-4">
-              <Link href="mailto:oren@opanitch.com">
-                <Email2Icon />
-              </Link>
-            </ListItem>
-          </List>
-
-          <ContactForm className="mt-5" />
-        </Card>
+        <div className="flex flex-col items-center justify-between md:flex-row md:items-start">
+          <Card className="w-5/6 md:w-1/2 md:mr-1">
+            <Header
+              className="mb-2"
+              headerLevel={2}
+              title={`{{@ cms.contact.title @}}`}
+            />
+            <p className="font-nugo">{`{{@ cms.contact.description @}}`}</p>
+            <List className="flex justify-center mt-3 leading-none text-scale-6">
+              <ListItem>
+                <Link
+                  href="https://www.linkedin.com/in/opanitch/"
+                  target="_blank"
+                >
+                  <LinkedinIcon />
+                </Link>
+              </ListItem>
+              <ListItem className="ml-4">
+                <Link href="https://github.com/opanitch" target="_blank">
+                  <GithubIcon />
+                </Link>
+              </ListItem>
+              <ListItem className="ml-4">
+                <Link href="mailto:oren@opanitch.com">
+                  <Email2Icon />
+                </Link>
+              </ListItem>
+            </List>
+          </Card>
+          <Card className="w-5/6 md:w-1/2 md:ml-1">
+            <ContactForm />
+          </Card>
+        </div>
       </ChildContainer>
     )}
   </FullWidthContainer>
