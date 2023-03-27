353322bc1d368e42016991c8e46990fc486e13c1
Oren Panitch <oreokid200@gmail.com>
2023-03-27 13:01:05 -0400

    refactor resume page to resize Resume content height better

--- source/containers/resume/resume.container.tsx
+++ source/containers/resume/resume.container.tsx
@@ -1,19 +1,46 @@
-import React, { FunctionComponent, SyntheticEvent } from 'react';
+import React, { FunctionComponent, useEffect, useRef } from 'react';
 import ResumeHTML from 'resume/resume.html';
 
-const setiFrameHeight = (event: SyntheticEvent): void => {
-  const iFrame = event.currentTarget as HTMLIFrameElement;
-
-  iFrame.style.height = `${iFrame.contentWindow?.document.body.scrollHeight}px`;
-};
-
 export const ResumeContainer: FunctionComponent = () => {
+  // plugins
+  const iFrameRef = useRef<HTMLIFrameElement>(null);
+
+  // utils
+  const checkContentHeight = (): void => {
+    const iFrame = iFrameRef.current;
+
+    if (iFrame) {
+      // get iFrame content size
+      const contentHeight = iFrame.contentWindow?.document.body.offsetHeight;
+
+      // only set height if we have a number
+      if (contentHeight) {
+        // set iFrame height
+        iFrame.style.height = `${contentHeight + 80}px`;
+      }
+    }
+  };
+
+  // side effects
+  useEffect(() => {
+    // keep track of content height
+    window.addEventListener('resize', checkContentHeight, {
+      passive: true,
+    });
+
+    // clean up on un-mount
+    return () => {
+      window.removeEventListener('resize', checkContentHeight);
+    };
+  }, []);
+
   // render
   return (
     <iframe
       className="w-full"
+      ref={iFrameRef}
       src={ResumeHTML}
-      onLoad={(event) => setiFrameHeight(event)}
+      onLoad={checkContentHeight}
     />
   );
 };
